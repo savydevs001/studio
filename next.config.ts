a5898ae34ae54811fdc6abf0699779bc172fffb6
Bilal Gul Sher <expertdevcs20@gmail.com>
2025-12-05 15:48:48 +0000

    won't that affect me when i deploy to another domain? so add a provsion

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,52 @@
 import type {NextConfig} from 'next';
 
+const appHostname = process.env.NEXT_PUBLIC_APP_URL
+  ? new URL(process.env.NEXT_PUBLIC_APP_URL).hostname
+  : undefined;
+
+const remotePatterns: NextConfig['images']['remotePatterns'] = [
+  {
+    protocol: 'http',
+    hostname: 'mike.bilalgul.dpdns.org',
+    port: '',
+    pathname: '/**',
+  },
+  {
+    protocol: 'https',
+    hostname: 'placehold.co',
+    port: '',
+    pathname: '/**',
+  },
+  {
+    protocol: 'https',
+    hostname: 'images.unsplash.com',
+    port: '',
+    pathname: '/**',
+  },
+  {
+    protocol: 'https',
+    hostname: 'picsum.photos',
+    port: '',
+    pathname: '/**',
+  },
+];
+
+if (appHostname) {
+  remotePatterns.push({
+    protocol: 'http',
+    hostname: appHostname,
+    port: '',
+    pathname: '/**',
+  });
+  remotePatterns.push({
+    protocol: 'https',
+    hostname: appHostname,
+    port: '',
+    pathname: '/**',
+  });
+}
+
+
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
@@ -9,32 +56,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'mike.bilalgul.dpdns.org',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+    remotePatterns,
   },
 };
 
